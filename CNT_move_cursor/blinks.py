2db68f0b22172450c71179f610bfae1c0e4b7e1f
devyansh <37020063+devyansh@users.noreply.github.com>
2020-02-22 13:41:54 -0800

    muse added

---
+++ CNT_move_cursor/blinks.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+Estimate Relaxation from Band Powers
+
+This example shows how to buffer, epoch, and transform EEG data from a single
+electrode into values for each of the classic frequencies (e.g. alpha, beta, theta)
+Furthermore, it shows how ratios of the band powers can be used to estimate
+mental state for neurofeedback.
+
+The neurofeedback protocols described here are inspired by
+*Neurofeedback: A Comprehensive Review on System Design, Methodology and Clinical Applications* by Marzbani et. al
+
+Adapted from https://github.com/NeuroTechX/bci-workshop
+"""
+
+import numpy as np  # Module that simplifies computations on matrices
+import matplotlib.pyplot as plt  # Module used for plotting
+from pylsl import StreamInlet, resolve_byprop  # Module to receive EEG data
+import utils  # Our own utility functions
+from blinkFilter import filt
+from scipy import signal
+import pyautogui # for spacebar
+
+# Handy little enum to make code more readable
+
+
+class Band:
+    Delta = 0
+    Theta = 1
+    Alpha = 2
+    Beta = 3
+
+
+""" EXPERIMENTAL PARAMETERS """
+# Modify these to change aspects of the signal processing
+
+# Length of the EEG data buffer (in seconds)
+# This buffer will hold last n seconds of data and be used for calculations
+BUFFER_LENGTH = 5
+
+# Length of the epochs used to compute the FFT (in seconds)
+EPOCH_LENGTH = 0.2
+
+# Amount of overlap between two consecutive epochs (in seconds)
+OVERLAP_LENGTH = 0.1
+
+# Amount to 'shift' the start of each next consecutive epoch
+SHIFT_LENGTH = EPOCH_LENGTH - OVERLAP_LENGTH
+
+# Index of the channel(s) (electrodes) to be used
+# 0 = left ear, 1 = left forehead, 2 = right forehead, 3 = right ear
+INDEX_CHANNEL = [0]
+
+if __name__ == "__main__":
+
+    """ 1. CONNECT TO EEG STREAM """
+
+    # Search for active LSL streams
+    print('Looking for an EEG stream...')
+    streams = resolve_byprop('type', 'EEG', timeout=2)
+    if len(streams) == 0:
+        raise RuntimeError('Can\'t find EEG stream.')
+
+    # Set active EEG stream to inlet and apply time correction
+    print("Start acquiring data")
+    inlet = StreamInlet(streams[0], max_chunklen=12)
+    eeg_time_correction = inlet.time_correction()
+
+    # Get the stream info and description
+    info = inlet.info()
+    description = info.desc()
+
+    # Get the sampling frequency
+    # This is an important value that represents how many EEG data points are
+    # collected in a second. This influences our frequency band calculation.
+    # for the Muse 2016, this should always be 256
+    fs = int(info.nominal_srate())
+
+    """ 2. INITIALIZE BUFFERS """
+
+    # Initialize raw EEG data buffer
+    eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+    filter_state = None  # for use with the notch filter
+
+    # Compute the number of epochs in "buffer_length"
+    n_win_test = int(np.floor((BUFFER_LENGTH - EPOCH_LENGTH) /
+                              SHIFT_LENGTH + 1))
+
+    # Initialize the band power buffer (for plotting)
+    # bands will be ordered: [delta, theta, alpha, beta]
+    band_buffer = np.zeros((n_win_test, 4))
+
+    """ 3. GET DATA """
+
+    # The try/except structure allows to quit the while loop by aborting the
+    # script with <Ctrl-C>
+    print('Press Ctrl-C in the console to break the while loop.')
+
+    try:
+        # The following loop acquires data, computes band powers, and calculates neurofeedback metrics based on those band powers
+        while True:
+
+            """ 3.1 ACQUIRE DATA """
+            # Obtain EEG data from the LSL stream
+            eeg_data, timestamp = inlet.pull_chunk(
+                timeout=1, max_samples=int(SHIFT_LENGTH * fs))
+
+            # Only keep the channel we're interested in
+            ch_data = np.array(eeg_data)[:, INDEX_CHANNEL]
+
+            # Update EEG buffer with the new data
+            eeg_buffer, filter_state = utils.update_buffer(
+                eeg_buffer, ch_data, notch=True,
+                filter_state=filter_state)
+
+            """ 3.2 COMPUTE BAND POWERS """
+            # Get newest samples from the buffer
+            data_epoch = utils.get_last_data(eeg_buffer,
+                                             int(EPOCH_LENGTH * fs))
+            #print(data_epoch.shape)
+
+            matchFilt = signal.hilbert(filt)
+
+            matches = signal.correlate(matchFilt,data_epoch[:,0])
+
+            matchesAbs = np.abs(matches[:])
+
+            maxMatch = np.max(matchesAbs)/1e5
+            print(maxMatch)
+
+            if maxMatch > 4.5:
+                print('blink')  
+                pyautogui.press("space")
+     
+    except KeyboardInterrupt:
+        print('Closing!')
