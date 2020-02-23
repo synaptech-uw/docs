8ec5e42178e1edbfc66e4b8cd4c47924b07f3443
VitaTaf <BryanTan0204@Gmail.com>
2020-02-22 21:36:57 -0800

    Reimplement calibration function
    Uses averages of peaks and account for varying delays

--- CNT_move_cursor/blinks.py
+++ CNT_move_cursor/blinks.py
@@ -23,9 +23,10 @@ import pyautogui # for spacebar
 import datetime
 import MoveCursor as m
 import time
+import _thread
 # Handy little enum to make code more readable
 
-pyautogui.PAUSE = 0.01                      # Make sure there is no delay between consecutive values when the autogui library is used
+#pyautogui.PAUSE = 0.01                      # Make sure there is no delay between consecutive values when the autogui library is used
 
 class Band:
     Delta = 0
@@ -114,6 +115,84 @@ def calibrate(buffer, filter_state):
     print('----------------- Calibration done- Now you can mind control the cursor --------------------')
     return averagedValue
 
+def input_thread(flag):
+    input()
+    flag.append(True)
+
+def get_average_blink(eye_index):
+    eeg_buffer = np.zeros((int(fs * BUFFER_LENGTH), 1))
+    filter_state = None
+    eye = "left" if eye_index == INDEX_CHANNEL_LEFT else "right"
+    print("Please blink your " + eye + " eye. Press enter when done.")
+    maxMatches = []
+    stop_flag = []
+    _thread.start_new_thread(input_thread, (stop_flag,))
+    i = 10 # number of times to run after user presses enter
+    while i > 0:
+        if stop_flag:
+            i = i - 1
+        eeg_data, timestamp = inlet.pull_chunk(
+            timeout=1, max_samples=int(SHIFT_LENGTH * fs))
+
+        # Only keep the channel we're interested in
+        ch_data = np.array(eeg_data)[:, eye_index]
+
+        # Update EEG buffer with the new data
+        eeg_buffer, filter_state = utils.update_buffer(
+            eeg_buffer, ch_data, notch=True,
+            filter_state=filter_state)
+
+        """ 3.2 COMPUTE BAND POWERS """
+        # Get newest samples from the buffer
+        data_epoch = utils.get_last_data(eeg_buffer,
+                int(EPOCH_LENGTH * fs))
+        matchFilt = signal.hilbert(filt)
+        matches = signal.correlate(matchFilt,data_epoch[:,0])
+        matchesAbs = np.abs(matches[:])
+        maxMatches.append((np.max(matchesAbs)/1e5).astype(int))
+
+    maxMatchesIdx = np.argmax(maxMatches)
+    idx = maxMatchesIdx
+    peakValues = []
+    stop_flag = 1
+    check_right_side = False
+    while True:
+        if idx > 0 and idx < len(maxMatches) - 1:
+            idx = idx + (1 if stop_flag == 2 else -1)
+            possPeak = maxMatches[idx]
+            if maxMatches[maxMatchesIdx]*0.6 < possPeak:
+                peakValues.append(possPeak)
+            elif stop_flag == 2:
+                break
+            else:
+                check_right_side = True
+        elif stop_flag == 2:
+            break
+        else:
+            check_right_side = True
+        if check_right_side:
+            idx = maxMatchesIdx
+            stop_flag = 2
+            check_right_side = False
+
+    return peakValues
+
+
+def calibrate(rep):
+    maxMatches = np.zeros((2, rep))
+    maxMatchesLength = np.zeros((2, rep))
+    for eye in [0 , 1]: # left = 0, right = 1
+        for i in range(rep):
+            temp = get_average_blink(
+                INDEX_CHANNEL_LEFT if eye == 0 else INDEX_CHANNEL_RIGHT)
+            maxMatches[eye, i] = np.average(temp)
+            maxMatchesLength[eye, i] = len(temp)
+
+    return np.average(maxMatches[0, :]), \
+            np.average(maxMatches[1, :]), \
+            np.average(maxMatchesLength[0, :]), \
+            np.average(maxMatchesLength[1, :])
+
 
 if __name__ == "__main__":
 
@@ -165,8 +244,16 @@ if __name__ == "__main__":
     oldTimeL = datetime.datetime.now() # initialize time delta
     oldTimeR = datetime.datetime.now() # initialize time delta
 
-    averagedValue = calibrate(eeg_buffer_left, filter_state_left)
+    #calibrate
+    left_thresh, right_thresh, left_delay, right_delay = calibrate(3)
 
+    #normalize delay
+    left_delay = left_delay * 85
+    right_delay = right_delay * 40
+
+    print("left_thresh: %d   right_thresh: %d", left_thresh, right_thresh)
+    print("left_delay: %d   right_delay: %d", left_delay, right_delay)
+    input()
 
     try:
         # The following loop acquires data, computes band powers, and calculates neurofeedback metrics based on those band powers
@@ -199,9 +286,9 @@ if __name__ == "__main__":
             maxMatch = np.max(matchesAbs)/1e5
             maxMatchL = maxMatch
 
-            if maxMatch > averagedValue + 1.5:
+            if maxMatch > left_thresh:
                 newTime = datetime.datetime.now()
-                if (newTime - oldTimeL).total_seconds()*1000 > DELTA:
+                if (newTime - oldTimeL).total_seconds()*1000 > left_delay:
                     print('selector')  
                     new_cursor.start_action("L")
                 oldTimeL = newTime
@@ -231,9 +318,9 @@ if __name__ == "__main__":
 
             maxMatch = np.max(matchesAbs)/1e5
             maxMatchR = maxMatch
-            if maxMatch > averagedValue + 1.5:
+            if maxMatch > right_thresh:
                 newTime = datetime.datetime.now()
-                if (newTime - oldTimeR).total_seconds()*1000 > DELTA:
+                if (newTime - oldTimeR).total_seconds()*1000 > right_delay:
                     print('executor')  
                     new_cursor.start_action("R")
                 oldTimeR = newTime
