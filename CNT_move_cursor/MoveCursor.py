fee614d65aff3bf2ef92de7e8edad482703bd6a0
devyansh <37020063+devyansh@users.noreply.github.com>
2020-02-23 20:05:15 -0800

    Implement clicking

--- CNT_move_cursor/MoveCursor.py
+++ CNT_move_cursor/MoveCursor.py
@@ -10,7 +10,7 @@ class MoveCursor:
 
     def __init__(self):
         super().__init__()
-        self._direction_list = ["Up", "Right", "Down", "Left"]
+        self._direction_list = ["Up", "Right", "Down", "Left", "Select"]
         self._current_dir_index = 3
         self._command = ""
         self.threads = []
@@ -42,8 +42,9 @@ class MoveCursor:
             Point(TRIANGLE_SIZE, WIDTH/2-TRIANGLE_SIZE)])
         right_arrow = Polygon([Point(WIDTH, WIDTH/2), Point(WIDTH-TRIANGLE_SIZE, WIDTH/2+TRIANGLE_SIZE),
             Point(WIDTH-TRIANGLE_SIZE, WIDTH/2-TRIANGLE_SIZE)])
+        circle = Circle(Point(WIDTH/2, HEIGHT/2), 50)
 
-        self.arrows = [up_arrow, right_arrow, down_arrow, left_arrow]
+        self.arrows = [up_arrow, right_arrow, down_arrow, left_arrow, circle]
         for a in self.arrows:
             a.setFill('blue')
             a.draw(self.win)
@@ -55,7 +56,7 @@ class MoveCursor:
         self._command = command
         if self._command == "L":
             self._current_dir_index += 1
-            if self._current_dir_index > 3:
+            if self._current_dir_index > 4:
                 self._current_dir_index = 0
             self.update_direction(self._current_dir_index)
             print("Direction Set:" + self._direction_list[self._current_dir_index])
@@ -70,8 +71,12 @@ class MoveCursor:
                     move.moveRel(0, 30, duration = 0.005)
                 elif curr_dir == "Left":
                     move.moveRel(-30, 0, duration = 0.005)
-                else:
+                elif curr_dir == "Right":
                     move.moveRel(30, 0, duration = 0.005)
+                else:
+                    move.click()
+                    move.click()
+                    self.movement = False
 
     def start_action(self, command):
         thread = Thread(target = self.action, args = (command, ))
